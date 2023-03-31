da23693c0ba5e675f26d12cda55cf2ef6b3e17eb
Ramila <rami061201@gmail.com>
2023-03-30 20:06:00 -0600

    modify the code

--- demoSpring/src/main/java/com/desarrollo/demoSpring/controllers/UsuarioController.java
+++ demoSpring/src/main/java/com/desarrollo/demoSpring/controllers/UsuarioController.java
@@ -1,25 +1,22 @@
 package com.desarrollo.demoSpring.controllers;
 
 
-
+import com.desarrollo.demoSpring.entities.Product;
+import com.desarrollo.demoSpring.services.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import com.desarrollo.demoSpring.services.UsuarioService;
-
-
-/**
- * Usuario controller.
- */
 @Controller
 public class UsuarioController {
+
     @Autowired
     private UsuarioService usuarioService;
+
     @RequestMapping("/login")
     public String showLogin(Model model) {
         return "login";
-    }
+	}
     @RequestMapping(value = "/login", method = RequestMethod.POST)
     public String login(@RequestParam("username") String name,
                         @RequestParam("pass") String pass) {
@@ -27,4 +24,6 @@ public class UsuarioController {
         if(isLogin) {return "index";}
         return"login";
     }
+
+
 }
